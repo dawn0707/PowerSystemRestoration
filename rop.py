da8046fac76efaa34b1092d3a45d1d88da219d6d
ChenZhang <39425798+dawn0707@users.noreply.github.com>
2019-03-23 22:22:09 -0600

    Add files via upload

--- rop.py
+++ rop.py
@@ -44,7 +44,12 @@ class ROP():
         self.y = self.model.addVars(self.item_num, self.r_num, vtype=GRB.BINARY, name="y")
         self.z = self.model.addVars(self.item_num, self.r_num, vtype=GRB.BINARY, name="z")
 
-        self.pl = self.model.addVars(self.pn.branch_num, self.r_num, vtype=GRB.CONTINUOUS, name="pl")
+        # self.pl = self.model.addVars(self.pn.branch_num, self.r_num, vtype=GRB.CONTINUOUS, name="pl")
+        for i in range(self.pn.branch_num):
+            var_name = "pl[" + str(i) + "]"
+            x = self.model.addVars(1, self.r_num, lb=0, ub=self.pn.branch[i][5], vtype=GRB.CONTINUOUS, name=var_name)
+            self.pl.append(x)
+
         for i in range(len(self.pn.gen)):
             var_name = "pv_gen[" + str(i) + "]"
             # x = self.model.addVars(1, self.r_num, lb=self.pn.gen[i][9], ub=self.pn.gen[i][8],
@@ -58,17 +63,19 @@ class ROP():
                                    vtype=GRB.CONTINUOUS, name=var_name)
             self.pv_load.append(x)
 
-        self.theta = self.model.addVars(self.pn.bus_num, self.r_num, lb=-math.pi/6, ub=math.pi/6,
+        self.theta = self.model.addVars(self.pn.bus_num, self.r_num, lb=-2 * math.pi, ub=2 * math.pi,
                                         vtype=GRB.CONTINUOUS, name="theta")
 
     def set_obj(self):
-        max_flow = 2850
+        max_flow = 1965.527380937
         obj = LinExpr()
+        # obj += max_flow - self.flow[0, self.r_num - 1]
         for i in range(self.r_num):
             obj += (max_flow - self.flow[0, i])
         self.model.setObjective(obj, GRB.MINIMIZE)
 
     def set_constraints(self):
+        self.model.addConstr(self.flow[0, self.r_num - 1] == 1965.527380937)
         """
         Constraint (2) in Model 3
         """
@@ -87,6 +94,8 @@ class ROP():
             con_name = "Constraint 3 " + str(k)
             for r in self.repair:
                 constr3 += self.o[r, k]
+            print("k")
+            print(k)
             self.model.addConstr(constr3 == k + 1, name=con_name)
 
         """
@@ -190,9 +199,9 @@ class ROP():
                 for index in gen_set:
                     expr += self.pv_gen[index][0, k]
                 for index in li_set:
-                    expr += self.pl[index, k]
+                    expr += self.pl[index][0, k]
                 for index in lo_set:
-                    expr -= self.pl[index, k]
+                    expr -= self.pl[index][0, k]
 
                 con_name = "Constraint 11 " + str(i) + " " + str(k)
                 self.model.addConstr(expr == 0, name=con_name)
@@ -200,40 +209,46 @@ class ROP():
         """
         Constraint (12) in Model 3
         """
+
         for k in range(self.r_num):
             for i in range(self.pn.gen_num):
                 con_name = "Constraint 12 gen " + str(i) + " " + str(k)
-                self.model.addConstr(self.pv_gen[i][0, k] <= self.pn.gen[i][8] * self.z[i + self.pn.bus_num, k],
-                                     name=con_name)
-                # self.model.addGenConstrIndicator(self.z[i + self.pn.bus_num, k], False,
-                #                                  self.pv_gen[i][0, k] == 0, name=con_name)
+                # self.model.addConstr(self.pv_gen[i][0, k] <= self.pn.gen[i][8] * self.z[i + self.pn.bus_num, k],
+                #                      name=con_name)
+                self.model.addGenConstrIndicator(self.z[i + self.pn.bus_num, k], False,
+                                                 self.pv_gen[i][0, k] == 0, name=con_name)
 
         for k in range(self.r_num):
             for i in range(self.pn.load_num):
                 con_name = "Constraint 12 load " + str(i) + " " + str(k)
                 # self.model.addConstr(self.pv_load[i][0, k] <= self.pn.load[i][2] *
                 #                      self.z[i + self.pn.bus_num + self.pn.load_num, k], name=con_name)
-                self.model.addGenConstrIndicator(self.z[i + self.pn.bus_num + self.pn.load_num, k], False,
+                self.model.addGenConstrIndicator(self.z[i + self.pn.bus_num + self.pn.gen_num, k], False,
                                                  self.pv_load[i][0, k] == 0, name=con_name)
 
         for k in range(self.r_num):
             for i in range(self.pn.branch_num):
                 con_name = "Constraint 9 " + str(i)
                 self.model.addGenConstrIndicator(self.z[i + self.pn.bus_num + self.pn.gen_num + self.pn.load_num, k],
-                                                 False, self.pl[i, k] == 0, name=con_name)
+                                                 False, self.pl[i][0, k] == 0, name=con_name)
 
         """
         Constraint 13 and 14 in Model (3)
         """
         for i in range(self.pn.branch_num):
-            to_bus = self.pn.branch[i][1] - 1
-            from_bus = self.pn.branch[i][0] - 1
-            for k in range(self.r_num):
-                con_name = "Constraint 13 " + str(i) + " " + str(k)
-                self.model.addGenConstrIndicator(self.z[i + self.pn.bus_num + self.pn.gen_num + self.pn.load_num, k],
-                                                 True, self.pl[i, k] ==
-                                                 self.pn.branch[i][4] * 100 * (self.theta[to_bus, k] -
-                                                                               self.theta[from_bus, k]))
+            if self.pn.branch[i][4] > 0:
+                to_bus = self.pn.branch[i][1] - 1
+                from_bus = self.pn.branch[i][0] - 1
+                for k in range(self.r_num):
+                    con_name = "Constraint 13 " + str(i) + " " + str(k)
+                    self.model.addGenConstrIndicator(
+                        self.z[i + self.pn.bus_num + self.pn.gen_num + self.pn.load_num, k],
+                        True, self.pl[i][0, k] ==
+                        self.pn.branch[i][4] * 100 * (self.theta[to_bus, k] - self.theta[from_bus, k]), name=con_name)
+
+        # self.model.addConstr(self.o[0, 0] == 1)
+        # self.model.addConstr(self.y[0, 0] == 1)
+        # self.model.addConstr(self.z[0, 0] == 1)
 
     def optimize(self):
         self.model.optimize()
@@ -252,15 +267,67 @@ class ROP():
     def display_o(self):
         o_value = []
         for i in self.repair:
-            if i in self.damaged_node:
-                value = []
-                for j in range(self.r_num):
-                    var_name = "y[" + str(i) + "," + str(j) + "]"
-                    var = self.model.getVarByName(var_name)
-                    value.append(var.x)
-                o_value.append(value)
+            value = []
+            for j in range(self.r_num):
+                var_name = "o[" + str(i) + "," + str(j) + "]"
+                var = self.model.getVarByName(var_name)
+                value.append(var.x)
+            o_value.append(value)
         print("o value:")
         print(o_value)
+
+    def display_z(self):
+        z_value = []
+        for i in range(self.item_num):
+            var_name = "z[" + str(i) + "," + str(self.r_num - 1) + "]"
+            var = self.model.getVarByName(var_name)
+            z_value.append(var.x)
+        print("z value:")
+        print(z_value)
+
+    def display_y(self):
+        y_value = []
+        for i in range(self.item_num):
+            var_name = "y[" + str(i) + "," + str(self.r_num - 1) + "]"
+            var = self.model.getVarByName(var_name)
+            y_value.append(var.x)
+        print("y value:")
+        print(y_value)
+
+    def get_sum_y(self):
+        sum_y_value = 0
+        for i in range(self.item_num):
+            var_name = "y[" + str(i) + "," + str(self.r_num - 1) + "]"
+            var = self.model.getVarByName(var_name)
+            sum_y_value = sum_y_value + var.x
+        return sum_y_value
+
+    def display_pv_load(self):
+        pv_load_value = []
+        for i in range(self.pn.load_num):
+            var_name = "pv_load[" + str(i) + "][0," + str(self.r_num - 1) + "]"
+            var = self.model.getVarByName(var_name)
+            pv_load_value.append(var.x)
+        print("stage 2 pv load:")
+        print(pv_load_value)
+
+    def display_pv_gen(self):
+        pv_gen_value = []
+        for i in range(self.pn.gen_num):
+            var_name = "pv_gen[" + str(i) + "][0," + str(self.r_num - 1) + "]"
+            var = self.model.getVarByName(var_name)
+            pv_gen_value.append(var.x)
+        print("stage 2 pv gen:")
+        print(pv_gen_value)
+
+    def display_pl(self):
+        pl_value = []
+        for i in range(self.pn.branch_num):
+            var_name = "pl[" + str(i) + "][0," + str(self.r_num - 1) + "]"
+            var = self.model.getVarByName(var_name)
+            pl_value.append(var.x)
+        print("stage 2 pl:")
+        print(pl_value)
 
 
 if __name__ == "__main__":
@@ -270,10 +337,28 @@ if __name__ == "__main__":
     # d_node = [0]
     stage1 = MRSP(pn, d_node)
 
-    minimum_repair_set = stage1.repair
-    # minimum_repair_set = [4,5 ,6]
+    # minimum_repair_set = stage1.repair
+    minimum_repair_set = [1, 14, 15]
     stage2 = ROP(pn, minimum_repair_set, d_node)
     stage1.display_repair()
     stage2.display_o()
+    y_sum = stage2.get_sum_y()
+    print("sum y:")
+    print(y_sum)
+    print(stage2.item_num)
+    stage1.display_z()
+    stage2.display_z()
+    stage1.display_y()
+    stage2.display_y()
+    stage1.display_pv_load()
+    stage2.display_pv_load()
+    stage1.display_pv_gen()
+    stage2.display_pv_gen()
+    stage1.display_pl()
+    stage2.display_pl()
+    print(stage2.pn.bus_num)
+    print(stage2.pn.gen_num)
+    print(stage2.pn.load_num)
+
 else:
     print("rop is implemented into another module.")
